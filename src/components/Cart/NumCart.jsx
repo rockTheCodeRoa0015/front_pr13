ea35e48be21a9a661bdf7ca3085ea4b6670efeb3
Jesus <roa0015@gmail.com>
2024-05-08 12:28:08 +0200

    numero carrito
    cambiar comprabacion para que no sea NaN y pete

--- src/components/Cart/NumCart.jsx
+++ src/components/Cart/NumCart.jsx
@@ -2,22 +2,22 @@ import { useContext, useEffect } from 'react'
 import StyledNumCart from './NumCart.style'
 import { NumCartContext } from '../../provider/NumCartProvider'
 import { getCartByPersonalId } from '../../api/cartApi'
-import { LoginContext } from '../../provider/LoginProvider'
 
 const NumCart = () => {
   const { numCart, setNumCart } = useContext(NumCartContext)
-  const { isLogin } = useContext(LoginContext)
 
   useEffect(() => {
-    if (localStorage.getItem('id_pr13Jroa') && isLogin) {
+    if (localStorage.getItem('id_pr13Jroa')) {
       setCartNum()
     }
   }, [])
 
   const setCartNum = async () => {
     const num = await getCartByPersonalId(localStorage.getItem('id_pr13Jroa'))
-    if (num !== 0 && num !== NaN) {
-      setNumCart(num)
+    if (!isNaN(num)) {
+      if (num !== 0) {
+        setNumCart(num)
+      }
     }
   }
   return <StyledNumCart>{numCart}</StyledNumCart>
